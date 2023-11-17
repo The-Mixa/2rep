0ef48468c9d8f031bd6ef34b50400839e697be70
Kirichenko <misha.oleg548@gmail.com>
2023-11-17 17:53:07 +0500

    finally files

--- main.py
+++ main.py
@@ -1,29 +1,14 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
 from random import randint
 import sys
 
 class CurcleDrawer(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setupUi(self)
+        uic.loadUi('UI.ui', self)
         self.do_paint = False
-
-    def setupUi(self, Form):
-        Form.setObjectName("Form")
-        Form.resize(400, 450)
-        self.pushButton = QtWidgets.QPushButton(Form)
-        self.pushButton.setGeometry(QtCore.QRect(20, 400, 361, 41))
-        self.pushButton.setObjectName("pushButton")
         self.pushButton.clicked.connect(self.paint)
 
-        self.retranslateUi(Form)
-        QtCore.QMetaObject.connectSlotsByName(Form)
-
-
-    def retranslateUi(self, Form):
-        _translate = QtCore.QCoreApplication.translate
-        Form.setWindowTitle(_translate("Form", "Form"))
-        self.pushButton.setText(_translate("Form", "Рисовать круг"))
 
     def paintEvent(self, event):
         if self.do_paint:
